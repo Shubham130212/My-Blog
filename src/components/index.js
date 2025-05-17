a06ce31cea03a3fedb868b2ff88936d2a85ad0d6
Shubham130212 <ladshubham1326@gmail.com>
2025-05-17 22:28:13 +0530

    pages-created

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,11 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
 import Button from "./Button";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
 import Input from "./Input";
 import Select from "./Select";
 
-export { Header, Footer, Container, Logo, LogoutBtn,RTE,Button,Input,Select };
+export { Header, Footer, Container, Logo, LogoutBtn,RTE,Button,Signup,Login,PostForm,PostCard,Input,Select}; 
